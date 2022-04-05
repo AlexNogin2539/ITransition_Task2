24afac4a58c3fcf840a19d36c951d607722a9f02
AlexNogin2539 <alexnogin.ru@gmail.com>
2022-04-05 21:02:23 +0300

    Init commit

---
+++ Task2_ITransition/DataExtractor.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+using System.Threading;
+
+namespace Task2_ITransition
+{
+    public class DataExtractor
+    {
+        public byte [] GetBinaryData()
+        {
+            
+            using (BinaryReader reader = new BinaryReader(File.Open("Sample/file_00.data", FileMode.Open)))
+            {
+                int count = 0;
+                while (reader.PeekChar() > -1)
+                {
+                    byte data1 = reader.ReadByte();
+                    count++;
+                }
+                byte[] data = new byte[count];
+                while (reader.PeekChar() > -1)
+                {
+                    data = reader.ReadBytes(count);
+                }
+                return data;
+            }
+        }
+
+        public void PutBinaryData(byte [] data)
+        {
+            using (var writer = new BinaryWriter(File.Open("Sample/new.data", FileMode.OpenOrCreate)))
+            {
+                writer.Write(data);
+            }
+        }
+    }
+}
