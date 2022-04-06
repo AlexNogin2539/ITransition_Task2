6347750ad1283c6cddeaa7aa0330ddf6abe825d0
AlexNogin2539 <alexnogin.ru@gmail.com>
2022-04-06 22:48:40 +0300

    ref

--- Task2_ITransition/Program.cs
+++ Task2_ITransition/Program.cs
@@ -1,17 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
-using System.Security.Cryptography;
 using SHA3.Net;
 
 namespace Task2_ITransition
 {
     internal class Program
     {
-
         static void Main(string[] args)
         {
             List<string> HashList = new List<string>();
@@ -20,27 +16,15 @@ namespace Task2_ITransition
             foreach (string file in files)
             {
                 byte[] data = File.ReadAllBytes(file);
-
-                var sb = new StringBuilder();
-
-                for (int i = 0; i < data.Length; i++)
-                {
-                    sb.Append(data[i].ToString());
-                }
-
-                string stringOfBytes = sb.ToString();
-
                 using (var shaAlg = Sha3.Sha3256())
                 {
-                    var hash = shaAlg.ComputeHash(Encoding.UTF8.GetBytes(stringOfBytes));
-
+                    var hash = shaAlg.ComputeHash(data);
                     var strb = new StringBuilder();
                     for (int i = 0; i < hash.Length; i++)
                     {
                         strb.Append(hash[i].ToString("x2"));
                     }
                     Console.WriteLine($"Hash({file}): " + strb.ToString());
-
                     HashList.Add(strb.ToString());
                 }
             }
@@ -48,13 +32,10 @@ namespace Task2_ITransition
             HashList.Sort();
 
             string result = string.Join("", HashList);
-
             string final = string.Concat(result, "alexnogin.ru@gmail.com");
-
             using (var shaAlg = Sha3.Sha3256())
             {
                 var finalHash = shaAlg.ComputeHash(Encoding.UTF8.GetBytes(final));
-
                 var stringBuilder = new StringBuilder();
                 for (int i = 0; i < finalHash.Length; i++)
                 {
@@ -64,6 +45,8 @@ namespace Task2_ITransition
                 Console.WriteLine("Final hash is: " + stringBuilder.ToString());
             }
 
+            Console.WriteLine("Press any key to close the programm. . .");
+            Console.ReadKey();
         }
     }
 }
